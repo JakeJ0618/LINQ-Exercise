5ddbf975710ec8061a4038323059e9121def5473
Jake Johnston <jakejohnston618@gmail.com>
2023-03-28 11:47:08 -0500

    made a list of games

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGameNames = new List<string> 
+            { 
+                "Call of Duty", 
+                "Star Wars: Battlefront 2", 
+                "Dead by Daylight", 
+                "Gears of War",
+                "Dishonored 2"
+            };
+
+            var listOfGames = videoGameNames.OrderBy(name => name.Length);
+
+            foreach(var gameName in listOfGames)
+            {
+                Console.WriteLine(gameName);
+            }
         }
     }
 }
